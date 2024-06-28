f27dae0a7bea4dc29c12f418dd215fd8ec428f8e
binodlamiochhane31 <lamibinod31@gmail.com>
2024-06-28 10:01:12 +0545

    search keyword case insensetive

--- src/main/java/com/example/myblogsite/repository/PostRepository.java
+++ src/main/java/com/example/myblogsite/repository/PostRepository.java
@@ -7,11 +7,14 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post,Long> {
     Page<Post> findByUser(User user,Pageable pageable);
     Page<Post> findByCategory(Category category, Pageable pageable);
-    List<Post> findByTitleContaining(String keyword);
+
+    @Query(value = "SELECT * FROM posts WHERE LOWER(title) LIKE LOWER(CONCAT('%', :keyword, '%'))",nativeQuery = true)
+    List<Post> findByTitleContaining(@Param("keyword") String keyword);
 }
